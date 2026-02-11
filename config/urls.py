705e3bdec90a46fd72a16ab497bea88912b433f5
Dathan Williams <121563204+Natasthedog@users.noreply.github.com>
2026-02-11 14:12:19 +0100

    Add File Uploads landing page and centralize uploads

--- config/urls.py
+++ config/urls.py
@@ -19,11 +19,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", RedirectView.as_view(url="/deck-automation/", permanent=False), name="root"),
-    path("", include("deck.urls")),
     path("", include("deck_automation.urls")),
+    path("", include("deck.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- deck_automation/urls.py
+++ deck_automation/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.file_uploads, name="file-uploads"),
     path("deck-automation/", views.deck_automation, name="deck-automation"),
     path(
         "deck-automation/download/<uuid:download_id>/",
