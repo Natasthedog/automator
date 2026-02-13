8d02bbc5d9211d7ad3d28d167dfac3427046c0af
Dathan Williams <121563204+Natasthedog@users.noreply.github.com>
2026-02-13 09:14:39 +0100

    Handle non-string column names in waterfall target normalization

--- deck/urls.py
+++ deck/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("product-description/", views.product_description, name="product-description"),
+    path("preqc-bprv/", views.preqc_bprv, name="preqc-bprv"),
 ]
